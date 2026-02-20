0fee5480164ad170b23aab149b1a390aee31781c
Jay Varshney <varshneyjay2@gmail.com>
2026-02-20 15:13:10 +0530

    every err fixed backend now running all repo and entities created

--- hackathon/src/main/java/com/hackathon/hackathon/controllers/mainController.java
+++ hackathon/src/main/java/com/hackathon/hackathon/controllers/mainController.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@Controller     
+@Controller
 public class mainController {
     @GetMapping("/")
     public String getMethodName() {
